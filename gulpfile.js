cc36f2cdf8a3e6eb2fe8cfff57b950eb1c47589a
Brian O'Sullivan <brian.osullivan@live.com>
2016-09-26 14:05:08 +0100

    Adding Elixir-Vueify and Normalize.css to dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const elixir = require('laravel-elixir');
 
 require('laravel-elixir-vue');
-
+require('laravel-elixir-vueify');
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
@@ -14,6 +14,8 @@ require('laravel-elixir-vue');
  */
 
 elixir(mix => {
-    mix.sass('app.scss')
-       .webpack('app.js');
+    mix.browserify('main.js')
+       .styles([
+          './node_modules/normalize.css/normalize.css'
+       ]);
 });
