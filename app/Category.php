569c4ba3e0cbdc2f588f56db4b2837a9c4bb9736
Brian O'Sullivan <brian.osullivan@live.com>
2016-09-27 02:00:09 +0100

    Organising the data exposed by each model

--- app/Category.php
+++ app/Category.php
@@ -6,8 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+  protected $appends = ['numberOfTopics'];
+  protected $visible = ['id', 'name', 'description', 'numberOfTopics'];
+
   public function topics()
   {
     return $this->hasMany(Topic::class);
+  }
+
+  public function getNumberOfTopicsAttribute()
+  {
+    return $this->topics()->count();
   }
 }
--- app/Topic.php
+++ app/Topic.php
@@ -6,8 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Topic extends Model
 {
+  protected $appends = ['time'];
+
+  protected $visible = ['id', 'title', 'body', 'views', 'time', 'cateogry_id'];
+
   public function category()
   {
     return $this->belongsTo(Category::class);
+  }
+
+  public function getTimeAttribute()
+  {
+    return $this->created_at->timestamp;
   }
 }
