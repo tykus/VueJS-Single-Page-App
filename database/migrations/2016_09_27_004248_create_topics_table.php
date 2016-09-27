b4bb900b285edb5f3d4be0eb3592cc3fce58af28
Brian O'Sullivan <brian.osullivan@live.com>
2016-09-27 01:46:35 +0100

    Setting up models, relations and migrations

---
+++ database/migrations/2016_09_27_004248_create_topics_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTopicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('topics', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('category_id')->unsigned();
+            $table->string('title');
+            $table->text('body');
+            $table->integer('views')->default(0);
+            $table->timestamps();
+
+            $table->foreign('category_id')
+                  ->references('id')
+                  ->on('categories')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('topics');
+    }
+}
