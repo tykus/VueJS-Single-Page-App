555890a19ce02c68439bf21e278b47ff44ce004c
Brian O'Sullivan <brian.osullivan@live.com>
2016-09-27 08:53:48 +0100

    Requesting categories data from the API

--- resources/assets/js/components/HomeView.vue
+++ resources/assets/js/components/HomeView.vue
@@ -3,3 +3,21 @@
     This is the homepage.
   </div>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        categories: []
+      }
+    },
+    route: {
+      data () {
+        return this.$http.get('/api/categories')
+          .then(function (data) {
+              return { categories: data.data }
+          });
+      }
+    }
+  }
+</script>
--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import Resource from 'vue-resource';
 import App from './components/App.vue';
 import HomeView from './components/HomeView.vue';
 
 Vue.use(Router);
+Vue.use(Resource);
 
 var router = new Router({
   history: true
